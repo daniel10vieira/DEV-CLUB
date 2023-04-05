6d29be6245f7f03fc222aa2bdc2eb3cb5d9e7e17
daniel10vieira <danielvieiraalcantara10@gmail.com>
2023-04-04 22:11:40 -0300

    .

---
+++ JavaScript Web/selecionando Elementos.js
@@ -0,0 +1,36 @@
+/*---------------- Selecionar Elementos -----------------*/
+
+//const element = document.getElementById("main-text")
+
+//const element = document.getElementsByClassName("paragraph")
+
+//const element = document.getElementsByTagName('h1')
+
+//const element = document.querySelector('p') //--> Sempre Retorna o primeiro que ele encontrar.
+
+//const element = document.querySelectorAll('p')
+
+// const element = document.querySelector('img') 
+
+/* const element = document.querySelector('h1') */
+
+/*---------------- Textos -----------------*/
+
+/* element.innerText = "Hello Word!"*/
+
+/* element.TextContent = "Hello Word!"*/
+
+/* element.innerHTML = "Hello Word" */
+
+/*---------------- CSS -----------------*/
+
+/* const element = document.querySelector('h1') */
+
+/* element.style.color = 'red'  */
+
+/* element.style.background = 'blue' */
+
+/*---------------- CSS - Eventos -----------------*/
+
+const element = document.querySelector('button')
+
